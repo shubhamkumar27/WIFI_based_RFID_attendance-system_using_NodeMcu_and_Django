aa4edddd7dd01d5819269d47fa3d42ec3a04ac9d
Shubham Kumar <shubhamkumar8180323@gmail.com>
2020-05-05 12:39:53 +0530

    Update urls.py

--- rfid_project/rfid_project/urls.py
+++ rfid_project/rfid_project/urls.py
@@ -1,5 +1,4 @@
 """rfid_project URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
